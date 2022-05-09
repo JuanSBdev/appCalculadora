b3041901499780ba01537c1d1016f0d56c64f2a9
JuanSBdev <juansb.dev@gmail.com>
2022-05-08 21:47:36 -0300

    mejorandoflexbox

--- jscalculadora/app.js
+++ jscalculadora/app.js
@@ -5,9 +5,9 @@ function sumar(){
     let resultado = parseInt(operando1.value)  + parseInt(operando2.value);
 if(isNaN(resultado)){
     resultado = "Fallo al calcular";
-    document.getElementById('resultado').innerHTML =  ` El resultado es: ${resultado}` }
+    document.getElementById('resultado').innerHTML =  ` r: ${resultado}` }
     else{
-        document.getElementById('resultado').innerHTML =  ` El resultado es: ${resultado}` };
+        document.getElementById('resultado').innerHTML =  ` r: ${resultado}` };
         console.log(`resultado: ${resultado}`);
     }
  function multiplicar(){
@@ -15,14 +15,14 @@ if(isNaN(resultado)){
      let operando1 = forma1['operandoA'];
      let operando2 = forma1['operandoB'];
      let resultado2 = parseInt(operando1.value) * parseInt(operando2.value);
-     document.getElementById('resultado').innerHTML =  ` El resultado es: ${resultado2}`
+     document.getElementById('resultado').innerHTML =  ` r: ${resultado2}`
      }
      function dividir(){
         let forma1 = document.getElementById('forma');
         let operando1 = forma1['operandoA'];
         let operando2 = forma1['operandoB'];
         let resultado3 = parseInt(operando1.value) / parseInt(operando2.value);
-        document.getElementById('resultado').innerHTML =  ` El resultado es: ${resultado3}`
+        document.getElementById('resultado').innerHTML =  ` r: ${resultado3}`
 
      }
   function ver(){
@@ -30,6 +30,6 @@ if(isNaN(resultado)){
     let operando1 = forma1['operandoA'];
     let operando2 = forma1['operandoB'];
     let resultado4 = parseInt(operando1.value) % parseInt(operando2.value);
-    document.getElementById('resultado').innerHTML =  ` El resultado es: ${resultado4}`
+    document.getElementById('resultado').innerHTML =  ` R: ${resultado4}`
   }
  
